21ffb3479432a8166128d8fc966cc26b0540b5ca
R.Grouls <rgrouls@gmail.com>
2023-07-01 22:58:21 +0200

    cli

---
+++ datascience_cookiecutter/cli.py
@@ -0,0 +1,21 @@
+import click
+from .cookiecutter import CookiecutterSettings, Cookiecutter
+from pathlib import Path
+
+
+@click.command()
+@click.argument("projectname")
+def main(projectname: str) -> None:
+    # Set the default settings
+    settings = CookiecutterSettings(
+        name=projectname,
+        path=Path("."),
+        git=True,
+    )
+
+    # Create the cookiecutter instance and execute
+    cookiecutter = Cookiecutter(settings)
+    cookiecutter()
+
+if __name__ == "__main__":
+    main()
